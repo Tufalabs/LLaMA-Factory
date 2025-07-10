1423963e01f0128d709d6243ac9665177ff78d5e
Isaiah Pressman <isaiahpressman16@gmail.com>
2025-07-10 17:02:58 -0400

    misc dataset updates

--- src/llamafactory/dataset_loaders/names.py
+++ src/llamafactory/dataset_loaders/names.py
@@ -3,9 +3,9 @@ from enum import Enum
 
 class DatasetName(Enum):
     OPEN_THOUGHTS_2 = "open-thoughts/OpenThoughts2-1M"
+    WEB_BACK_TRANSLATION = "YuxinJiang/web_back_translation_100k_llama3"
     WEBR_BASIC = "YuxinJiang/WebR-Basic-100k"
     WEBR_PRO = "YuxinJiang/WebR-Pro-100k"
-    WEB_BACK_TRANSLATION = "YuxinJiang/web_back_translation_100k_llama3"
 
     @property
     def full_path(self) -> str:
